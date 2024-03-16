afb2a67b839a2caad26c9ce0a637a8b30338599e
Mohd Uvaish <mohammadbadshah92@gmail.com>
2024-03-16 16:08:41 +0530

    Upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function openURL(Link) {
+    if(Link === 'JoinMe')
+    {
+        window.open('https://www.youtube.com/channel/UCcT79p6kqC_OvQgYh7YHkkw/join');
+    }
+    if(Link === 'YouTube')
+    {
+        window.open('https://www.youtube.com/channel/UCcT79p6kqC_OvQgYh7YHkkw');
+    }
+    if(Link === 'Instagram')
+    {
+        window.open('https://www.instagram.com/itsmeprinceyt/');
+    }
+    if(Link === 'Discord')
+    {
+        window.open('https://discord.gg/HgXNs4p5cx');
+    }
+    if(Link === 'GitHub')
+    {
+        window.open('https://github.com/itsmeprinceyt');
+    }
+  }
